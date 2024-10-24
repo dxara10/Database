236ee07b7cf75da76affb33287368d27e44b3733
Douglas Cortes <135165192+dxara10@users.noreply.github.com>
2024-10-24 15:09:13 -0300

    Create CRUDRead.py

---
+++ CRUDRead.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+conexao = mysql.connector.connect(
+    host="localhost",
+    user="seu_usuario",
+    password="sua_senha",
+    database="seu_banco_de_dados"
+)
+
+cursor = conexao.cursor()
+
+# Ler dados de uma tabela
+sql = "SELECT * FROM clientes"
+cursor.execute(sql)
+
+resultados = cursor.fetchall()
+
+for resultado in resultados:
+    print(resultado)
+
+conexao.close()
